85395c9dfb837b63c78460e126613796470c99f8
Bret Curtis <psi29a@gmail.com>
2011-03-14 17:07:25 +0100

    Cleaned up code, added progress bar because sometimes... you just do not know.

--- mapMaker.py
+++ mapMaker.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
+# Export from an image map information based on system characters such cp437.
 
 import os, errno
+import sys
 from PIL import Image
 from hashlib import md5
+from progressbar import ProgressBar, Percentage, ETA
 
 def mkdir_p(path):
     try:
@@ -11,14 +14,20 @@ def mkdir_p(path):
         if exc.errno == errno.EEXIST:
             pass
         else: raise
+
+def RGBToHTMLColor(rgb_tuple):
+    """ convert an (R, G, B) tuple to #RRGGBB """
+    hexcolor = '#%02x%02x%02x' % rgb_tuple
+    # that's it! '%02x' means zero-padded, 2-digit hex values
+    return hexcolor
         
 
 # parameters 
 locationDir = "/opt/df/"
-legendSave = locationDir+"legend/"
+legendSave = locationDir+"legend"+os.sep
 locationFile = "worldMap.bmp"
 elementSize = [8,12]
-locationFonts = os.getcwd()+"/fonts/"
+locationFonts = os.getcwd()+os.sep+"fonts"+os.sep
 font = "cp437-8x12.pbm"
 fontGlyphs = 256
 
@@ -42,13 +51,17 @@ for y in range(0,fontSize[1],elementSize[1]):
 elements = elementsGlyph = {}
 internalMap = []
 outputUTF = ""
-outputHTMLplain = "<pre>\n"
+outputHTMLplain = "<body style='background-color:#000000'><pre>\n"
 outputHTMLimg = "<p style='width:2056px;'>"
+
+# setup progressbar
+widgets = ['Processing: ', Percentage(), ' ', ETA() ]
+pbar = ProgressBar(widgets=widgets, maxval=(mapSize[1]/elementSize[1]+1)*(mapSize[0]/elementSize[0])+1)
 
 x = y = 0
 for offsetY in range(0,mapSize[1],elementSize[1]):
     for offsetX in range(0,mapSize[0],elementSize[0]):
-        #print offsetX,offsetY
+        # get offsetX,offsetY
         element = mapObject.crop([offsetX,offsetY,offsetX+elementSize[0],offsetY+elementSize[1]])
         md5sum = md5(str(list(element.getdata()))).hexdigest()
         # if map element is unique, export
@@ -57,34 +70,41 @@ for offsetY in range(0,mapSize[1],elementSize[1]):
             # if map element matches supplied font file, export extra details
             foundFont = False
             glyphCode = 0;
+            colour = (0,0,0)
             for fontElement in fontElements:
                 # since our font glyphs are in black and white (1bit), we need
                 # convert the map glyph from 24-bit colour to grayscale, then
                 # from grayscale to (1bit) using point function to take anything
                 # not black and set to white.
                 if list(fontElement.getdata()) == list(element.convert("L").point(lambda i: i * 20,"1").getdata()):
-                    print "Found a match for map element: "+chr(glyphCode).decode('cp437')
+                    #print "Found a match for map element: "+chr(glyphCode).decode('cp437')
                     foundFont = True
                     break
                 glyphCode+=1
-                    
             if not foundFont:
                 glyphCode=-1
-                elements[md5sum]=glyphCode
                 print "Could not find glyph for map element: "+md5sum
-            elements[md5sum]=glyphCode
-        else:
-            glyphCode = elements[md5sum]        
+                
+            # find the colour of the glyph, as it could have additional meaning.
+            for pixelColour in list(element.getdata()):
+                if not pixelColour == colour: # pure black
+                    colour = pixelColour
+                    break
+                
+            elements[md5sum]=glyphCode,colour  
         
         internalMap.append((x,y,md5sum,glyphCode))
-        outputUTF+=chr(glyphCode).decode('cp437').encode('utf-8')
-        outputHTMLplain+=chr(glyphCode).decode('cp437').encode('ascii', 'xmlcharrefreplace')
+        outputUTF+=chr(elements[md5sum][0]).decode('cp437').encode('utf-8')
+        outputHTMLplain+="<font color='"+RGBToHTMLColor(elements[md5sum][1])+"'>"+chr(elements[md5sum][0]).decode('cp437').encode('ascii', 'xmlcharrefreplace')+"</font>"
         outputHTMLimg+="<img src='"+legendSave+md5sum+".bmp"+"' style='border:none; margin:0px; padding:0px;'/>"
         x+=1
     outputUTF+="\n"
     outputHTMLplain += "\n"
     outputHTMLimg += "<br/>"    
     y+=1
+    #print x+y
+    pbar.update(x+y)
+
 outputHTMLplain += "</pre>\n"
 outputHTMLimg += "</p>\n"
     
@@ -99,3 +119,6 @@ fileObject.close()
 fileObject = open(locationDir+locationFile+".htm","w")
 fileObject.write(outputHTMLimg)
 fileObject.close()
+
+pbar.finish()
+sys.stdout.write('Finished\n')
